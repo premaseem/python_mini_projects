9e30e9ae725a3efd35e670b648c19fb0ab356d6c
Aseem Jain <Aseem.Jain@rackspace.com>
2016-03-09 00:28:48 -0600

    Main file  for bulk rename program

---
+++ Bulk_rename/Main.py
@@ -0,0 +1,12 @@
+__author__ = 'premaseem'
+
+import os
+
+list_of_files = os.listdir("/Users/asee2278/gitRepo/petProjects/python_mini_projects/Bulk_rename/prank")
+print list_of_files
+current_working_dir = os.getcwd()
+os.chdir("/Users/asee2278/gitRepo/petProjects/python_mini_projects/Bulk_rename/prank")
+for file in list_of_files :
+    new_name = file.translate(None,"0123456789")
+    os.renames(file,new_name)
+    print new_name
