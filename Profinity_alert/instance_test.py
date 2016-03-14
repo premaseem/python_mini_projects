72fafadfa41cc0be0e43127f1de1cff4e99c6d89
Aseem Jain <Aseem.Jain@rackspace.com>
2016-03-13 23:59:04 -0500

    instance variable test to become local variable

---
+++ Profinity_alert/instance_test.py
@@ -0,0 +1,14 @@
+__author__ = 'asee2278'
+
+
+class Person:
+    def __init__(self, age_param, sex_param):
+        self.age = age_param
+        sex = sex_param
+
+male = Person(22,"male")
+#print male.sex
+
+female = Person(16,"female")
+print sex
+
